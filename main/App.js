8a08deac00b73d6530981bc8b6a102929ac79a94
KalabusChipopino <vadim1454@gmail.com>
2023-10-09 02:05:11 +0300

    Update App.js
    try1

--- main/App.js
+++ main/App.js
@@ -4,8 +4,8 @@ import './App.css';
 export function App() {
     return (
         <div className='p-16 bg-blue'>
-            <h1>CCCCCCCC</h1>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rem quos delectus voluptatem ab repellat saepe eos molestiae cupiditate, omnis doloremque, necessitatibus totam consectetur porro sint quam libero aperiam? Laborum, quaerat?</p>
+            <h1>kick a baby</h1>
+            <p> quaerat?</p>
         </div>
     );
 }
