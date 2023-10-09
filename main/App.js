6f35299870d495ad0607f198432025cc41d30c6d
KalabusChipopino <vadim1454@gmail.com>
2023-10-09 21:14:21 +0300

    Update App.js
    funny text  updated to a not funny text

--- main/App.js
+++ main/App.js
@@ -4,7 +4,7 @@ import './App.css';
 export function App() {
     return (
         <div className='p-16 bg-blue'>
-            <h1>kick a baby</h1>
+            <h1>world, hello</h1>
             <p> quaerat?</p>
         </div>
     );
