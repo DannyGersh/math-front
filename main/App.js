a066f0e809fe03b9abe1d9d890194dfba3abb5d9
KalabusChipopino <vadim1454@gmail.com>
2023-10-09 21:35:58 +0300

    Update App.js
    checking if dev changes and main is not

--- main/App.js
+++ main/App.js
@@ -4,7 +4,7 @@ import './App.css';
 export function App() {
     return (
         <div className='p-16 bg-blue'>
-            <h1>world, hello</h1>
+            <h1>world,</h1>
             <p> quaerat?</p>
         </div>
     );
